47d1554c7020c7b3e570c7462a1514c95a82b213
Nguyen Thi Lan Anh <81799232+lananh20111999@users.noreply.github.com>
2022-08-14 05:07:00 +0700

    ok

---
+++ textDetection/textDetection.py
@@ -0,0 +1,103 @@
+import torch
+import torch.backends.cudnn as cudnn
+from torch.autograd import Variable
+import cv2
+import numpy as np
+from textDetection import craft_utils
+from textDetection import imgproc
+from textDetection.craft import CRAFT
+
+from collections import OrderedDict
+def copyStateDict(state_dict):
+    if list(state_dict.keys())[0].startswith("module"):
+        start_idx = 1
+    else:
+        start_idx = 0
+    new_state_dict = OrderedDict()
+    for k, v in state_dict.items():
+        name = ".".join(k.split(".")[start_idx:])
+        new_state_dict[name] = v
+    return new_state_dict
+
+
+def test_net(net, image, text_threshold, link_threshold, low_text, cuda, poly, canvas_size, mag_ratio, refine_net=None):
+    # resize
+    img_resized, target_ratio, size_heatmap = imgproc.resize_aspect_ratio(image, canvas_size, interpolation=cv2.INTER_LINEAR, mag_ratio= mag_ratio)
+    ratio_h = ratio_w = 1 / target_ratio
+
+    # preprocessing
+    x = imgproc.normalizeMeanVariance(img_resized)
+    x = torch.from_numpy(x).permute(2, 0, 1)    # [h, w, c] to [c, h, w]
+    x = Variable(x.unsqueeze(0))                # [c, h, w] to [b, c, h, w]
+    if cuda:
+        x = x.cuda()
+
+    # forward pass
+    with torch.no_grad():
+        y, feature = net(x)
+
+    # make score and link map
+    score_text = y[0,:,:,0].cpu().data.numpy()
+    score_link = y[0,:,:,1].cpu().data.numpy()
+
+    # refine link
+    if refine_net is not None:
+        with torch.no_grad():
+            y_refiner = refine_net(y, feature)
+        score_link = y_refiner[0,:,:,0].cpu().data.numpy()
+
+    # Post-processing
+    boxes, polys = craft_utils.getDetBoxes(score_text, score_link, text_threshold, link_threshold, low_text, poly)
+
+    # coordinate adjustment
+    boxes = craft_utils.adjustResultCoordinates(boxes, ratio_w, ratio_h)
+    polys = craft_utils.adjustResultCoordinates(polys, ratio_w, ratio_h)
+    for k in range(len(polys)):
+        if polys[k] is None: polys[k] = boxes[k]
+
+    # render results (optional)
+    render_img = score_text.copy()
+    render_img = np.hstack((render_img, score_link))
+    ret_score_text = imgproc.cvt2HeatmapImg(render_img)
+
+    return boxes, polys, ret_score_text
+
+
+
+def text_detection(img, trained_model='textDetection/craft_mlt_25k.pth', text_threshold=0.7, low_text=0.4, link_threshold=0.4, cuda=False, canvas_size=1280, mag_ratio=1.5, poly=False,
+                    refine=False, refiner_model='weights/craft_refiner_CTW1500.pth'):
+    # load net
+    net = CRAFT()     # initialize
+    if cuda:
+        net.load_state_dict(copyStateDict(torch.load(trained_model)))
+    else:
+        net.load_state_dict(copyStateDict(torch.load(trained_model, map_location='cpu')))
+
+    if cuda:
+        net = net.cuda()
+        net = torch.nn.DataParallel(net)
+        cudnn.benchmark = False
+
+    net.eval()
+
+    # LinkRefiner
+    refine_net = None
+    if refine:
+        from refinenet import RefineNet
+        refine_net = RefineNet()
+        # print('Loading weights of refiner from checkpoint (' + refiner_model + ')')
+        if cuda:
+            refine_net.load_state_dict(copyStateDict(torch.load(refiner_model)))
+            refine_net = refine_net.cuda()
+            refine_net = torch.nn.DataParallel(refine_net)
+        else:
+            refine_net.load_state_dict(copyStateDict(torch.load(refiner_model, map_location='cpu')))
+
+        refine_net.eval()
+        poly = True
+
+
+    bboxes, polys, score_text = test_net(net, img, text_threshold, link_threshold, low_text, cuda, poly, canvas_size, mag_ratio, refine_net)
+
+    return bboxes, polys, score_text
+
